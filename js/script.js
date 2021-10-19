146ca4d9711fe538cf3c505e59715f63ea3218aa
MwitaBrian <brianmwita13@gmail.com>
2021-10-20 02:31:26 +0300

    Modified Js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ function displayAkanName(){
   var month = dateEntered.getMonth()
   var day = dateEntered.getDate()
 
+  //Get gender value
+  var gender = "female"
+
   //Get the century and Year
     //Convert year to string
     var yearString = year.toString()
@@ -16,13 +19,25 @@ function displayAkanName(){
     var DD = day
 
     //Use the given formula from LMS
-    dayOfTheWeek = parseInt(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+    dayOfTheWeek = parseInt( ( (CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD)%7
     console.log(dayOfTheWeek);
-
-
 
   //array for male Names
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   //array for female Names
   var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+  
+  if (gender === "male"){
+    var maleAkan = maleNames[dayOfTheWeek]
+    console.log(maleAkan)
+    alert("Your Ghanaian name is " +  maleAkan)
+  }
+  else if(gender === "female"){
+    var femaleAkan = femaleNames[dayOfTheWeek]
+    console.log(femaleAkan)
+    alert("Your Ghanaian name is " + femaleAkan)
+  }
+  else{
+    console.log("Too bad choose a gender")
+  }
 }

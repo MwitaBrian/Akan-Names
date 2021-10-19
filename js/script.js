4491bc0e18523c7bc7b91771bc86323415889686
MwitaBrian <brianmwita13@gmail.com>
2021-10-19 23:35:31 +0300

    Add array for female names

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 //array for male Names
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+//array for female Names
+var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"]

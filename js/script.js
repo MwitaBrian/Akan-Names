4606be1875acb9cd78e806f3cbd059e6b3c36f08
MwitaBrian <brianmwita13@gmail.com>
2021-10-20 00:00:05 +0300

    Get birthday value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+function displayAkanName(){
+//Get birthday value
+var birthday = document.querySelector('input[type="date"]').value;
+console.log(birthday)
+
 //array for male Names
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 //array for female Names
 var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+}

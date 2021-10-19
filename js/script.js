9a2ce50bebb6bd581d6b7867d1f38655494b36c2
MwitaBrian <brianmwita13@gmail.com>
2021-10-20 00:30:58 +0300

    Convert year from Number to string

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ function displayAkanName(){
   var year = dateEntered.getFullYear()
   var month = dateEntered.getMonth()
   var day = dateEntered.getDate()
+  console.log(typeof year + ' year ')
+
+  //Get the century and Year
+    //Convert year to string to get century
+    var yearString = year.toString()
 
   //array for male Names
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];

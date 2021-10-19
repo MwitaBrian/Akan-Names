4212340499d78c6b29a22507cb94c58bca53a2e9
MwitaBrian <brianmwita13@gmail.com>
2021-10-20 00:58:06 +0300

    calculate day of the week using given formula

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 function displayAkanName(){
   //Get birthday value
   var birthday = document.querySelector('input[type="date"]').value;
-  console.log(birthday);
   var dateEntered = new Date(birthday);
-  console.log(dateEntered);
   var year = dateEntered.getFullYear()
   var month = dateEntered.getMonth()
   var day = dateEntered.getDate()
-  console.log(typeof year + ' year ')
 
   //Get the century and Year
-    //Convert year to string to get century
+    //Convert year to string
     var yearString = year.toString()
-
+    //use Slice method then convert back to Integer
     var CC = parseInt(yearString.slice(0,2))
     var YY = parseInt(yearString.slice(2,4))
     var MM = month
     var DD = day
 
-    console.log(CC)
-    console.log(YY)
+    //Use the given formula from LMS
+    dayOfTheWeek = parseInt(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+    console.log(dayOfTheWeek);
+
 
 
   //array for male Names

09980687034c98523dc9ac74f1720766b5d4665b
MwitaBrian <brianmwita13@gmail.com>
2021-10-20 00:16:41 +0300

    Create Date object and get variables using Date methods

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function displayAkanName(){
-//Get birthday value
-var birthday = document.querySelector('input[type="date"]').value;
-console.log(birthday)
+  //Get birthday value
+  var birthday = document.querySelector('input[type="date"]').value;
+  console.log(birthday);
+  var dateEntered = new Date(birthday);
+  console.log(dateEntered);
+  var year = dateEntered.getFullYear()
+  var month = dateEntered.getMonth()
+  var day = dateEntered.getDate()
 
-//array for male Names
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-//array for female Names
-var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+  //array for male Names
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  //array for female Names
+  var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 }
